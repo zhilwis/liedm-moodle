072b58d2cf8c0d3278d3e8e01dc2827347931dbe
Zilvinas Vaira <zilvinas.vaira@ik.ku.lt>
2014-03-28 20:25:09 +0200

    Versijos reikalavimų atstatymas.

--- report/coursequality/version.php
+++ report/coursequality/version.php
@@ -13,6 +13,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014031000;    // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014031000;    // Requires this Moodle version
+$plugin->version   = 2013110500;    // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013110500;    // Requires this Moodle version
 $plugin->component = 'report_coursequality';  // Full name of the plugin (used for diagnostics)
